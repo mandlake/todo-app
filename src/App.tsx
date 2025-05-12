275f76d9f60c77d48ef2e9f3dfcf95b20bf59c6e
Sion Kim <94289633+mandlake@users.noreply.github.com>
2025-05-12 12:52:14 +0900

    start todo-app with vite & react & typescript & tailwind css

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl text-red-500 font-bold">
+        Tailwind 색상 적용 확인!
+      </h1>
+    </div>
+  );
+}
